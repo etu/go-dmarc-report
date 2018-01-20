cfacacad5584f5b4dfff1d57a4e2229cc44571e3
Elis Hirwing <elis@hirwing.se>
2018-01-20 21:26:06 +0100

    Draw a better table

--- dmarcreport-printer.go
+++ dmarcreport-printer.go
@@ -2,32 +2,47 @@ package main
 
 import (
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"net"
-	"time"
+	"os"
 )
 
 func printDmarcFeedbacks(dfs []DmarcFeedback) {
+	table := tablewriter.NewWriter(os.Stdout)
+
+	table.SetHeader([]string{
+		"IP Address",
+		"Hostname",
+		"Message Count",
+		"Disposition",
+		"DKIM Domain",
+		"DKIM Result",
+		"SPF Domain",
+		"SPF Result",
+	})
+
 	for _, df := range dfs {
-		fmt.Printf(
-			"%s -- %s\n",
-			time.Unix(df.ReportMetadata.DateRange.Begin, 0),
-			time.Unix(df.ReportMetadata.DateRange.End, 0),
-		)
+		//fmt.Printf(
+		//	"%s -- %s\n",
+		//	time.Unix(df.ReportMetadata.DateRange.Begin, 0),
+		//	time.Unix(df.ReportMetadata.DateRange.End, 0),
+		//)
 
 		for _, rec := range df.Record {
 			hostnames, _ := net.LookupAddr(rec.Row.SourceIP)
 
-			fmt.Printf(
-				"%15s | %s | %d | %s | %30s | %s | %30s | %s\n",
+			table.Append([]string{
 				rec.Row.SourceIP,
 				hostnames[0],
-				rec.Row.Count,
+				fmt.Sprint(rec.Row.Count),
 				rec.Row.PolicyEvaluated.Disposition,
 				rec.AuthResults.DKIM.Domain,
 				rec.AuthResults.DKIM.Result,
 				rec.AuthResults.SPF.Domain,
 				rec.AuthResults.SPF.Result,
-			)
+			})
 		}
 	}
+
+	table.Render()
 }
