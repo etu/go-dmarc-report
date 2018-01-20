afafb170ee5f17274e1f6da0d98222605d1017f2
Elis Hirwing <elis@hirwing.se>
2018-01-20 18:05:49 +0100

    Gather all reports before printing

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 func main() {
 	files := os.Args[1:]
 
-	for _, file := range files {
-		feedback := parseDmarcXML(getZipFileContents(file))
+	var dfs []DmarcFeedback
 
-		fmt.Printf("Contents of %s:\n", file)
-		fmt.Println(feedback)
+	for _, file := range files {
+		dfs = append(
+			dfs,
+			parseDmarcXML(getZipFileContents(file)),
+		)
 	}
+
+	fmt.Println("All parsed reports from all files:")
+	fmt.Println(dfs)
 }
